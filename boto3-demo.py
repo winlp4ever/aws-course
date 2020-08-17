e791957c2026653e183cfa83ed4c53f4159857bb
ha quang le <winlp4ever@gmail.com>
2020-08-17 23:44:58 +0200

    update demo boto3

--- boto3-demo.py
+++ boto3-demo.py
@@ -1,11 +1,8 @@
 import boto3
 # use s3 service
 s3 = boto3.resource('s3')
-response = client.add_user_to_group(
-    GroupName='string',
-    UserName='string'
-)
 
 # upload file to s3
 data = open('text.txt', 'rb')
-s3.Bucket('mypicture-1234').put_object(Key='txt.txt', Body=data, ContentType='text/plain')
+s3.Bucket('my-files-123').put_object(Key='text-files/text.txt', Body=data, ContentType='text/plain')
+data.close()
